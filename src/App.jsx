50fea06790d12c305645cb589fc38e6934bf086a
isabellacastro02 <isabella.mcastro02@gmail.com>
2026-04-14 09:44:56 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,61 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import logo from './assets/logo.png'
 
-export default function App() { 
+function App() {
+  const [count, setCount] = useState(0)
 
-  const MATERIAS = [
-    { nome: "Matemática", emoji: "📐"},
-    { nome: "Português", emoji: "📚"},
-    { nome: "História", emoji: "🏛️"},
-    { nome: "Ciências", emoji: "🧪"},
-  ];
-
-  const [nome, setNome] = useState(""); //texto
-  const [notas, setNotas] = useState({
-    "Matemática": 9.5,
-    "Português": 6,
-    "História": 8,
-    "Ciências": 4 }); //objeto
-  const [materia, setMateria] = useState(MATERIAS[0]);
-  const notaAtual = notas[materia.nome];
-  const aprovado = notaAtual >= 6;
-
- 
   return (
     <>
-      <main>
-        <section>
+    <main>
+      <img src={logo} alt="Logo" className="logo" />
+      
+        <label for="usuario">USUÁRIO</label>
+        <input type="text" placeholder="Digite seu usuário" />
 
-          <img src={logo} alt="Logo" className="logo" />
+        <label for="senha">SENHA</label>
+        <input type="password" placeholder="Digite sua senha" />
 
-          <div className="thumb">
-            {aprovado ? materia.emoji : "❌"}
-          </div>
+        <div className="opcoes">
+          <a href="#">esqueceu a senha?</a>
+        </div>
 
-          <input
-            type="text"
-            className='nome'
-            placeholder='Nome do Aluno'
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          />
-
-          <div className="status">
-            <p>{materia.nome}</p>
-            <p>Nota: {notaAtual}/10</p>
-            <span>{aprovado ? "APROVADO" : "RECUPERAÇÃO"}</span>
-            <span>{aprovado ? "ACIMA DA MÉDIA" : "ABAIXO DA MÉDIA"}</span>
-          </div>
-
-          <div className="classes">
-            <button onClick={() => setMateria(MATERIAS[0])}>📐 Matemática</button>
-            <button onClick={() => setMateria(MATERIAS[1])}>📚 Português</button>
-            <button onClick={() => setMateria(MATERIAS[2])}>🏛️ História</button>
-            <button onClick={() => setMateria(MATERIAS[3])}>🧪 Ciências</button>
-          </div>
-
-        </section>
+        <button type="submit">ENTRAR</button>
       </main>
     </>
-  )
+  );
 }
+export default App;
